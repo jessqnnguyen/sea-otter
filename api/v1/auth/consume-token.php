cfecdcf33d5021f7a2a57e5e85f448f81ee7ab9c
Luke Kaalim <lukekaalim@gmail.com>
2016-02-09 06:29:59 +1100

    added comments to php code

--- api/v1/auth/consume-token.php
+++ api/v1/auth/consume-token.php
@@ -1,15 +1,20 @@
 <?php
+	//Require the authenication library
 	require(getenv("DOCUMENT_ROOT")."/v1/lib/authlib.php");
 	
+	//If the client isn't POSTing, don't even bother
 	if($_SERVER['REQUEST_METHOD'] != "POST") {
 		print("Error: not POST request.");
 		exit();
 	}
 	
+	//Allow the cross-domain flags for local testing
 	header("Access-Control-Allow-Origin: http://127.0.0.1:81");
 	
+	//Reply to this request with an array
 	$reply = array();
 	
+	//if the token is valid, consume it and give the client back their new token.
 	if(isset($_POST["token"])) {
 		
 		$reply["token"] = $auth->ConsumeToken($_POST["token"]);
@@ -20,5 +25,6 @@
 		$reply["success"] = false;
 	}
 	
-	print(json_encode($reply));
+	//Allow the cross-domain flags for local testing
+	header("Access-Control-Allow-Origin: http://127.0.0.1:81");
 ?>
