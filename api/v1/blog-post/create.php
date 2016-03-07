4cb328224d762c69a3ce4af0ffd6a7edb6deba79
Jess <jessqnnguyen@gmail.com>
2016-03-08 00:09:07 +1100

    updated create blog post script to handle saving multiple blog posts, also outputs the current array of blog posts to the test post submission page

--- api/v1/blog-post/create.php
+++ api/v1/blog-post/create.php
@@ -6,40 +6,66 @@
  		exit();
  	}
 
+ 	// Store the post data.
  	$newPost = $_POST["blog-content"];
 
+ 	// Check if the post data is non-null and is actually set.
  	if(isset($newPost)) {
  		
+ 		// Get the directory path of the JSON file which stores all the blog post data.
  		$postFileName = getenv("DOCUMENT_ROOT")."/../local/blogpostfile.json";
+ 		$fileExists = file_exists($postFileName);
+ 		$postFile = createBlogPostFileHandle($postFileName, $fileExists);
+ 		$fileSize = filesize($postFileName);
  		
- 		if (!file_exists($postFileName)) {
+ 		$contents;
+ 		$blogPosts = [];
+ 		
+ 		// Check if file size is larger than 0 or not empty.
+ 		if ($fileSize > 0) {
+ 			// Read the existing blog post file and store in a string.
+ 			$contents = fread($postFile, $fileSize);
+ 			// TODO(Jess): Delete later - for debugging.
+ 			// print($contents);
+ 			// Parse the contents JSON string into an array.
+ 			$blogPosts = json_decode($contents, true) ? : [];
+ 			print($blogPosts);
+ 			// Clear the blog post file.
+ 			ftruncate($postFile, 0);
+ 			// Update the pointer back to the beginning of the file.
+ 			fseek($postFile, 0);
+ 		} 
+ 		// Push the new blog post to the array of blog posts.
+ 		array_push($blogPosts, $newPost);
+ 		// Encode and store back into a JSON string.
+ 		$postJSONString = json_encode($blogPosts);
+ 		// Update the post file to the new JSON string.
+ 		fwrite($postFile, $postJSONString);
 
- 		} else {
- 			// Open for reading and writing; place the file pointer at the beginning 
-	 		// of the file and truncate the file to zero length. If the 
-	 		// file does not exist, attempt to create it.
-	 		$postFile = fopen($postFileName, "r+");
- 		}
-
- 		$postFile = fopen($postFileName, "r+");
- 		
- 		fwrite($postFile, $newPost);
- 		
  		fclose($postFile);
 
- 		print($newPost); 
- 	} else {
- 	  
- 	  print("Error: no post data.");
- 	  exit();
-
+ 		// TODO(Jess): Delete later - for debugging.
+ 		// print($newPost); 
+ 	} else { 
+ 	    print("Error: no post data.");
+ 	    exit();
  	}
 
- 	function createFileHandle($fileName) {
- 		if (file_exists($fileName)) {
- 			$fileHandle = fopen($postFileName, "r+");
+ 	/**
+ 	 * Given a file name and whether the file currently exists or not,
+ 	 * returns a blog post file handle with the appropriate r/w mode.
+ 	 * @param fileName - string file name
+ 	 * @param fileExists - boolean state of whether file exists
+ 	 */
+ 	function createBlogPostFileHandle($fileName, $fileExists) {
+ 		if ($fileExists) {
+ 			// Open for reading and writing; place the file pointer at the beginning of the file.
+ 			$fileHandle = fopen($fileName, "r+");
  		} else {
- 			$fileHandle = fopen($postFileName, "a");
+ 			// Open for writing only; place the file pointer at the end of the file. 
+ 			// If the file does not exist, attempt to create it. 
+ 			$fileHandle = fopen($fileName, "a");
  		}
+ 		return $fileHandle;
  	}
 ?>
